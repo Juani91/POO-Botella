4869dd0c5d15b8a6bd18e63fd015b03ca1783b33
Juan Ignacio Zucco <juani_z29@hotmail.com>
2023-08-03 10:59:07 -0300

    Version 1

---
+++ Botella/Botella.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botella
+{
+    internal class Botella
+    {
+        // Creamos una Botella.
+        // Al crearla hay que darle las características de Material, Color, Capacidad y CantidadActual.
+        // Viene con una Capacidad de 1000ml
+        // Viene con una CantidadActual de 0ml
+        // Se puede recargar con un precio de $100 cada 1000ml
+
+        // ATRIBUTOS y PROPIEDADES
+
+        public string Material { get; }
+        public string Color { get; }
+        public int Capacidad { get; }
+        public int CantidadActual { get; set; } // Si el método va a cambiarlo debe tener set también. 
+
+
+        // CONSTRUCTORES
+
+        // Material y Color van a ser variables (se los damos al momento de crearlo)
+        // Capacidad y CantidadActual son fijos (siempre se creará una botella vacía de 1000ml)
+
+        public Botella(string Material, string Color)
+        {
+            this.Material = Material;
+            this.Color = Color;
+            Capacidad = 1000;
+            CantidadActual = 0;
+        }
+
+
+        // METODOS
+
+        // Hacer que se recargue la Botella
+        // Luego que se tome 1/4
+        // Y se vuelva a recargar (con el precio ajustado)
+
+        public int Recargar(int m)
+        {
+            int d;
+            if (CantidadActual > 0)
+            {
+                d = Capacidad - CantidadActual;
+                CantidadActual += d;
+                // 1000ml -> $100
+                // dif -> x=monto
+                m = (d * m) / 1000;
+                return m;
+            }
+            else
+            {
+                CantidadActual = 1000;
+                //m = m;
+                return m;
+            }
+        }
+
+        public void Beber()
+        {
+            CantidadActual -= 250;
+        }
+    }
+}
---
+++ Botella/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, monto = 100;
+
+            Botella B1 = new Botella("Plástico", "Transparente");
+
+            Console.WriteLine("El material de la botella es: " + B1.Material);
+            Console.WriteLine("El color de la botella es: " + B1.Color);
+            Console.WriteLine("La capacidad de la botella es: " + B1.Capacidad + "ml");
+            Console.WriteLine("La cantidad actual de la botella es: " + B1.CantidadActual + "ml");
+
+
+            //B1.Recargar(monto);
+            r = B1.Recargar(monto); // ESTABAS RECARGANDO 2 VECES! AL PONER "R=" TAMBIEN ESTABAS CITANDO A QUE SE RECARGUE DENUEVO
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Recargamos..");
+            Console.WriteLine("La cantidad actual de la botella es: " + B1.CantidadActual + "ml");
+            Console.WriteLine("El monto de la recarga es de: " + r);
+
+            B1.Beber();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Bebemos..");
+            Console.WriteLine("La cantidad actual de la botella es: " + B1.CantidadActual + "ml");
+
+            //B1.Recargar(monto);
+            r = B1.Recargar(monto);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Recargamos..");
+            Console.WriteLine("La cantidad actual de la botella es: " + B1.CantidadActual + "ml");
+            Console.WriteLine("El monto de la recarga es de: " + r);
+
+            Console.ReadKey();
+        }
+    }
+}
